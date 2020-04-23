4a4e8c2bb6f7ea26dc3530553a1cc93c80e568a8
SamuelMon <64022058+SamuelMon@users.noreply.github.com>
2020-04-23 00:30:05 -0500

    Archivos
    Primer commit

--- src/proyecto/pkgfinal/GUI.java
+++ src/proyecto/pkgfinal/GUI.java
@@ -5,12 +5,16 @@
  */
 package proyecto.pkgfinal;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author raulmontoyaarredondo
  */
 public class GUI extends javax.swing.JFrame {
 
+    public String[] question = new String[5];
     /**
      * Creates new form GUI
      */
@@ -18,6 +22,222 @@ public class GUI extends javax.swing.JFrame {
         initComponents();
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Inicio Metodos
+        
+    //Genera un caso aleartorio de la pregunta tipo 1
+    public String[] preguntaTipo1(){
+        Random rdn = new Random();
+        String[] question = new String[5];
+        String message = "";
+        int upperLimit = rdn.nextInt(2000)-990;
+        int lowerLimit = rdn.nextInt(2000)-1000;
+        int combination = rdn.nextInt(4);
+        int combination2 = rdn.nextInt(4);
+        int correctAnswer =0;
+        int option1 = 0;
+        int option2 = 0;
+        int option3 = 0;
+        String mensaje = "";
+        
+        
+        //Compruebo que el limitie superior sea mayor que el limite inferior, encaso de uqe nosea asi
+        //intercambio los valores de estos.
+        if (lowerLimit>upperLimit){
+            int x = upperLimit;
+            upperLimit = lowerLimit;
+            lowerLimit = x;
+        }
+        
+        // Utilizo Switch para tener en cuenta los posibles casos correspondientes a las combinaciones 
+        // estre Los parentesis y los corchetes, ya que estos definen si un limite se incluye en el rango
+        // o no y puede cambiar las opciones a elegir.
+        // El caso elegido dependera de una variable que tiene un valor random entre 0 y 3.
+        switch (combination){
+            case 0:
+            {
+                //Asigno a la variable correctAnswer un valor que se encuentre en el intervalo generado
+                correctAnswer = lowerLimit + rdn.nextInt(upperLimit - lowerLimit + 1);
+                message = "Cual de las siguientes opciones pertenece a intervalo definido por [ " + lowerLimit + " , " + upperLimit + "] .".toString();
+                
+                // Estos if sirven para tener en cuente la posibilidades a la hora de mostrar posibles 
+                // respuestas.
+                // El caso elegido dependera de una variable que tiene un valor random entre 0 y 3.
+                if (combination2 == 0 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 6;
+                    option2 = upperLimit + rdn.nextInt(5)+ 1;
+                    option3 = upperLimit + rdn.nextInt(50) + 26;
+                }
+
+                if (combination2 == 1 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = lowerLimit - rdn.nextInt(5)-1;
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 2 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = upperLimit + rdn.nextInt(5) + 1;
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 3 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 6;
+                    option2 = lowerLimit - rdn.nextInt(5);
+                    option3 = upperLimit + rdn.nextInt(50) + 26;
+                }  
+                break;
+            }
+            
+            case 1:
+                {
+                    //Asigno a la variable correctAnswer un valor que se encuentre en el intervalo generado
+                    correctAnswer = lowerLimit + rdn.nextInt(upperLimit - lowerLimit );
+                    message = "Cual de las siguientes opciones pertenece a intervalo definido por [ " + lowerLimit + " , " + upperLimit + ") .".toString();
+
+                    // Estos if sirven para tener en cuente la posibilidades a la hora de mostrar posibles 
+                    // respuestas.
+                    // El caso elegido dependera de una variable que tiene un valor random entre 0 y 3.
+                    if (combination2 == 0 )
+                    {
+                        option1 = upperLimit + rdn.nextInt(20) + 5;
+                        option2 = upperLimit + rdn.nextInt(5);
+                        option3 = upperLimit + rdn.nextInt(50) + 25;
+                    }
+
+                    if (combination2 == 1 )
+                    {
+                        option1 = lowerLimit - rdn.nextInt(20) - 6;
+                        option2 = lowerLimit - rdn.nextInt(5) - 1;
+                        option3 = lowerLimit - rdn.nextInt(50) - 26;
+                    }
+
+                    if (combination2 == 2 )
+                    {
+                        option1 = lowerLimit - rdn.nextInt(20) - 5;
+                        option2 = upperLimit + rdn.nextInt(5);
+                        option3 = lowerLimit - rdn.nextInt(50) - 25;
+                    }
+
+                    if (combination2 == 3 )
+                    {
+                        option1 = upperLimit + rdn.nextInt(20) + 5;
+                        option2 = lowerLimit - rdn.nextInt(5) - 1;
+                        option3 = upperLimit + rdn.nextInt(50) + 25;
+                    }  
+                    break;
+                }
+            
+            case 2:
+            {
+                //Asigno a la variable correctAnswer un valor que se encuentre en el intervalo generado
+                correctAnswer = 1 + lowerLimit + rdn.nextInt(upperLimit - lowerLimit );
+                message = "Cual de las siguientes opciones pertenece a intervalo definido por ( " + lowerLimit + " , " + upperLimit + "] .".toString();
+                
+                // Estos if sirven para tener en cuente la posibilidades a la hora de mostrar posibles 
+                // respuestas.
+                // El caso elegido dependera de una variable que tiene un valor random entre 0 y 3.
+                if (combination2 == 0 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 5;
+                    option2 = upperLimit + rdn.nextInt(5);
+                    option3 = upperLimit + rdn.nextInt(50) + 25;
+                }
+
+                if (combination2 == 1 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = lowerLimit - rdn.nextInt(5);
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 2 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = upperLimit + rdn.nextInt(5);
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 3 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 6;
+                    option2 = lowerLimit - rdn.nextInt(5);
+                    option3 = upperLimit + rdn.nextInt(50) + 26;
+                }
+                break;
+            }
+            
+            case 3:
+            {
+                //Asigno a la variable correctAnswer un valor que se encuentre en el intervalo generado
+                correctAnswer = 1+ lowerLimit + rdn.nextInt(upperLimit - lowerLimit - 1);
+                message = "Cual de las siguientes opciones pertenece a intervalo definido por ( " + lowerLimit + " , " + upperLimit + ") .".toString();
+                
+                // Estos if sirven para tener en cuente la posibilidades a la hora de mostrar posibles 
+                // respuestas.
+                // El caso elegido dependera de una variable que tiene un valor random entre 0 y 3.
+
+                if (combination2 == 0 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 5;
+                    option2 = upperLimit + rdn.nextInt(5);
+                    option3 = upperLimit + rdn.nextInt(50) + 25;
+                }
+
+                if (combination2 == 1 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = lowerLimit - rdn.nextInt(5);
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 2 )
+                {
+                    option1 = lowerLimit - rdn.nextInt(20) - 6;
+                    option2 = upperLimit + rdn.nextInt(5);
+                    option3 = lowerLimit - rdn.nextInt(50) - 26;
+                }
+
+                if (combination2 == 3 )
+                {
+                    option1 = upperLimit + rdn.nextInt(20) + 6;
+                    option2 = lowerLimit - rdn.nextInt(5);
+                    option3 = upperLimit + rdn.nextInt(50) + 26;
+                }
+                break;    
+            }
+            
+            default:
+            {
+                JOptionPane.showMessageDialog(null,"Hubo un error al generar el numero random en la variable combination, porfavor reporte este error a los desarrolladores");
+                break;
+            }
+        }//Fin switch
+        
+        // Asigno a las posiciones de un vector todo lo necesario para imprimir en pantalla la pregunta generada    
+        question[0] = message;
+        question[1] = String.valueOf(correctAnswer);
+        question[2] = String.valueOf(option1);
+        question[3] = String.valueOf(option2);
+        question[4] = String.valueOf(option3);
+        return question;
+    }
+    // Fin Metodos
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,27 +247,17 @@ public class GUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jButton1.setText("jButton1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(153, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(150, 150, 150))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(96, 96, 96)
-                .addComponent(jButton1)
-                .addContainerGap(175, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -83,12 +293,11 @@ public class GUI extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUI().setVisible(true);
+            new GUI().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
